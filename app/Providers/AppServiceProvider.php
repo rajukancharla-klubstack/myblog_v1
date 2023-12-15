c4310f7de0fcd7adcbd1338898af8b036568a55e
Raju Kancharla <119564575+rajukancharla-klubstack@users.noreply.github.com>
2023-12-15 11:14:14 +0530

    Added External API Service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\ApiService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $apiUrl = config('services.external_api.url'); // Assuming you store the API URL in the configuration
+
+        $this->app->bind(ApiService::class, function ($app) use ($apiUrl) {
+            return new ApiService($apiUrl);
+        });
     }
 
     /**
